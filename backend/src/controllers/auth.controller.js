4258356bc55d30fdf58aaed47137da207031304d
AbdallahIsmaili <aismaili691@gmail.com>
2025-05-03 20:09:00 +0100

    Signup Endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,5 +1,57 @@
+import User from '../models/User.js';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcryptjs';
+
+
 export async function signup(req, res) {
-    res.send('User signed up!');
+    const {email, password, fullName} = req.body;
+
+    try {
+        if(!email || !password || !fullName) {
+            return res.status(400).json({message: 'Please fill all fields!'});
+        }
+        if(password.length < 8) {
+            return res.status(400).json({message: 'Password must be at least 8 characters!'});
+        }
+        const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        if(!emailRegex.test(email)) {
+            return res.status(400).json({message: 'Please enter a valid email!'});
+        }
+
+        const existingUser = await User.findOne({email});
+        if(existingUser) {
+            return res.status(400).json({message: 'Email already exists!'});
+        }
+
+        const idx = Math.floor(Math.random() * 100) + 1;
+        const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`;
+
+        const newUser = await User.create({
+            email,
+            password,
+            fullName,
+            profilePic: randomAvatar,
+        });
+
+
+        const token = jwt.sign({userId: newUser._id}, process.env.JWT_SECRET_KEY, { expiresIn: '7d' });
+
+
+        res.cookie("jwt", token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+            httpOnly: true,
+            sameSite: "strict",
+            secure: process.env.NODE_ENV === "production",
+        })
+
+        res.status(201).json({success: true, user: newUser, message: 'User created!'});
+
+    } catch(error) {
+
+        console.log("Error in signup controller: ", error);
+        return res.status(500).json({message: 'Internal server error!'});
+    }
+    
 }
 
 export async function login(req, res) {
---
+++ backend/src/models/User.js
@@ -0,0 +1,70 @@
+import mongoose from "mongoose";
+import bycrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      minlength: 8,
+    },
+    bio: {
+      type: String,
+      default: "Hey there! I am using this app.",
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+    nativeLanguage: {
+      type: String,
+      default: "",
+    },
+    learningLanguage: {
+      type: String,
+      default: "",
+    },
+    location: {
+      type: String,
+      default: "",
+    },
+    isOnboarded: {
+      type: Boolean,
+      default: false,
+    },
+    friends: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  { timestamps: true }
+);
+
+// pre hook to hash password before saving
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+
+  try {
+    const salt = await bycrypt.genSalt(10);
+    this.password = await bycrypt.hash(this.password, salt);
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+const User = mongoose.model("User", userSchema);
+
+export default User;
