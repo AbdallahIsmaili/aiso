0ac49a232c0ba3c5a6bd375d99c190003919b7e7
AbdallahIsmaili <aismaili691@gmail.com>
2025-05-03 20:51:12 +0100

    upsert users to Stream dashboard

---
+++ backend/src/lib/stream.js
@@ -0,0 +1,24 @@
+import {StreamChat} from 'stream-chat';
+import "dotenv/config"
+
+const apiKey = process.env.STREAM_API_KEY
+const apiSecret = process.env.STREAM_API_SECRET
+
+if(!apiKey || !apiSecret) {
+    console.error("STREAM_API_KEY or STREAM_API_SECRET is not set in the environment variables.")
+}
+
+const streamClient = StreamChat.getInstance(apiKey, apiSecret);
+
+
+export const upsertStreamUser = async (userData ) => {
+    try {
+        await streamClient.upsertUser(userData);
+        return userData;
+
+    }catch (error) {
+        console.error("Error in upsertStreamUser: ", error);
+    }
+}
+
+export const generateStreamToken = async (userId) => {};
