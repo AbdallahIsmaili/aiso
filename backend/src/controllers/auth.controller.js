2c66f60ad24b42e9cc3d13a00978e83c271ecc1c
AbdallahIsmaili <aismaili691@gmail.com>
2025-05-03 20:23:35 +0100

    Login and Logout Endpoint

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,9 +55,45 @@ export async function signup(req, res) {
 }
 
 export async function login(req, res) {
-    res.send('User logged in!');
+    try{
+        const {email, password} = req.body;
+
+        if(!email || !password) {
+            return res.status(400).json({message: 'Please fill all fields!'});
+        }
+
+        const user = await User.findOne({email});
+        if(!user) {
+            return res.status(401).json({message: 'Invalid credentials!'});
+        }
+
+        const isPasswordCorrect = await user.matchPassword(password);
+        if(!isPasswordCorrect) {
+            return res.status(401).json({message: 'Invalid credentials!'});
+        }
+
+        const token = jwt.sign(
+          { userId: user._id },
+          process.env.JWT_SECRET_KEY,
+          { expiresIn: "7d" }
+        );
+
+        res.cookie("jwt", token, {
+          maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
+          httpOnly: true,
+          sameSite: "strict",
+          secure: process.env.NODE_ENV === "production",
+        });
+
+        res.status(200).json({success: true, user, message: 'User logged in!'});
+
+    }catch(error) {
+        console.log("Error in login controller: ", error);
+        return res.status(500).json({message: 'Internal server error!'});
+    }
 }
 
 export function logout(req, res) {
-    res.send('User logged out!');
+    res.clearCookie("jwt")
+    res.status(200).json({success: true, message: 'User logged out!'});
 }
