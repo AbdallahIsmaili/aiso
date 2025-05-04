7fd9c1eb2264da416d4c1f68292d496dc176c9f3
AbdallahIsmaili <aismaili691@gmail.com>
2025-05-04 22:29:28 +0100

    protectRoute Middleware and updating Stream user data

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -148,6 +148,22 @@ export async function onboard(req, res) {
       return res.status(404).json({ message: "User not found!" });
     }
 
+    try {
+      await upsertStreamUser({
+        id: updatedUser._id.toString(),
+        name: updatedUser.fullName,
+        image: updatedUser.profilePic || "",
+      });
+
+      console.log(
+        `Stram user updated after onboarding for ${updatedUser.fullName}`
+      );
+    } catch (streamError) {
+      console.log(
+        "Error updating Stream user during onboarding", streamError.message
+      );
+    }
+
     res.status(200).json({
       success: true,
       user: updatedUser,
