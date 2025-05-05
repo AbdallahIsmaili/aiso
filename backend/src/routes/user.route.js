429470b969a00783fd91792cf2ac43cf7e3dca5b
AbdallahIsmaili <aismaili691@gmail.com>
2025-05-05 20:12:52 +0100

    Accept Friend Request Endpoint

--- backend/src/routes/user.route.js
+++ backend/src/routes/user.route.js
@@ -3,6 +3,7 @@ import {
   getMyFriends,
   getRecommendedUsers,
   sendFriendRequest,
+  acceptFriendRequest,
 } from "../controllers/user.controller.js";
 import { protectRoute } from "../middleware/auth.middleware.js"
 
@@ -17,6 +18,7 @@ router.get('/friends', getMyFriends);
 
 
 router.post("/friend-request/:id", sendFriendRequest);
+router.put("/friend-request/:id/accept", acceptFriendRequest);
 
 
 export default router;
