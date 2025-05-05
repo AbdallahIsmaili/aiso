691a1851e94c18830b9a954ed59fb59812cf014a
AbdallahIsmaili <aismaili691@gmail.com>
2025-05-05 19:52:09 +0100

    Get Recommended Users & Friends Endpoint

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,36 @@
+import User from "../models/User.js";
+
+export async function getRecommendedUsers(req, res) {
+  try {
+    const currentUserId = req.user.id;
+    const currentUser = req.user;
+
+    const recommendedUsers = await User.find({
+      $and: [
+        { _id: { $ne: currentUserId } },
+        { $id: { $nin: currentUser.friends } },
+        { isOnboarded: true },
+      ],
+    });
+    res.status(200).json({ recommendedUsers });
+  } catch (error) {
+    console.error("Error fetching recommended users:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
+
+export async function getMyFriends(req, res) {
+  try {
+    const user = await User.findById(req.user.id)
+      .select("friends")
+      .populate(
+        "friends",
+        "fullName profilePicture nativeLanguage learningLanguage"
+      );
+
+      res.status(200).json(user.friends );
+  } catch (error) {
+    console.error("Error fetching friends:", error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+}
