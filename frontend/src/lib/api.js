32d5e6bb6ea33f662580dec7e1fe927771eeae55
AbdallahIsmaili <aismaili691@gmail.com>
2025-05-07 20:05:45 +0100

    Onboarding Page

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -11,3 +11,8 @@ export const getAuthUser = async () => {
     return res.data;
   };
 };
+
+export const completeOnboarding = async (userData) => {
+  const response = await axiosInstance.post("/auth/onboarding", userData);
+  return response.data;
+};
