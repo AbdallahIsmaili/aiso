7a116f806ffd5cc725952be00e792dc1f6904ee2
AbdallahIsmaili <aismaili691@gmail.com>
2025-05-07 19:22:01 +0100

    Cleaning code and creating Hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,24 +8,15 @@ import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
 import { Toaster } from "react-hot-toast";
-import { axiosInstance } from "./lib/axios.js";
-import { useQuery } from "@tanstack/react-query";
 import PageLoader from "./components/PageLoader.jsx";
-
+import useAuthUser from "./hooks/useAuthUser.js";
 
 const App = () => {
-  const {
-    data: authData,
-    isLoading,
-  } = useQuery({
-    queryKey: ["authUser"],
-    queryFn: async () => {
-      const res = await axiosInstance.get("/auth/me");
-      return res.data;
-    },
-  });
+  
+  const {isLoading, authUser} = useAuthUser();
 
-  const authUser = authData?.user;
+  const isAuthenticated = Boolean(authUser);
+  const isOnboarded = authUser?.isOnboarded;
 
   if(isLoading) return <PageLoader />;
 
@@ -34,31 +25,41 @@ const App = () => {
       <Routes>
         <Route
           path="/"
-          element={authUser ? <HomePage /> : <Navigate to="/login" />}
+          element={
+            isAuthenticated && isOnboarded ? (
+              <HomePage />
+            ) : (
+              <Navigate to={isAuthenticated ? "/onboarding" : "/login" } />
+            )
+          }
         />
         <Route
           path="/signup"
-          element={!authUser ? <SignupPage /> : <Navigate to="/" />}
+          element={!isAuthenticated ? <SignupPage /> : <Navigate to="/" />}
         />
         <Route
           path="/login"
-          element={authUser ? <Navigate to="/" /> : <LoginPage />}
+          element={isAuthenticated ? <Navigate to="/" /> : <LoginPage />}
         />
         <Route
           path="/onboarding"
-          element={authUser ? <OnboardingPage /> : <Navigate to="/login" />}
+          element={
+            isAuthenticated ? <OnboardingPage /> : <Navigate to="/login" />
+          }
         />
         <Route
           path="/notifications"
-          element={authUser ? <NotificationsPage /> : <Navigate to="/login" />}
+          element={
+            isAuthenticated ? <NotificationsPage /> : <Navigate to="/login" />
+          }
         />
         <Route
           path="/call"
-          element={authUser ? <CallPage /> : <Navigate to="/login" />}
+          element={isAuthenticated ? <CallPage /> : <Navigate to="/login" />}
         />
         <Route
           path="/chat"
-          element={authUser ? <ChatPage /> : <Navigate to="/login" />}
+          element={isAuthenticated ? <ChatPage /> : <Navigate to="/login" />}
         />
       </Routes>
 
--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -3,4 +3,11 @@ import { axiosInstance } from "./axios";
 export const signup = async (signupData) => {
   const response = await axiosInstance.post("/auth/signup", signupData);
   return response.data;
-}
+};
+
+export const getAuthUser = async () => {
+  async () => {
+    const res = await axiosInstance.get("/auth/me");
+    return res.data;
+  };
+};
